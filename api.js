1a8c92133e60c4d6ed807482fb3af719c5e689b4
Pigges <pigges@tutanota.com>
2023-01-12 17:52:23 +0100

    added claim search function

--- api.js
+++ api.js
@@ -1,20 +1,24 @@
 import axios from 'axios';
 
-export default async (uri)=>{
+export default async function api (method, params, pages) {
     let data;
+    if (pages) params.page_size = 50;
     try {
         const resp = await axios({
             method: "post",
             url: process.env.LBRY_API,
             data: {
-                method: "get",
-                params: {
-                    uri
-                }
+                method,
+                params
             }
         });
-        data = resp.data;
+
+        
+        if (resp.data.error) return {error: resp.data.error.message};
+        data = resp.data.result;
+
     } catch(err) {
+        console.log(err);
         console.log("Could not talk to the LBRY SDK");
         data = {"message": "error"};
     }
--- index.js
+++ index.js
@@ -11,18 +11,50 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', async (req,res)=>{
-    let text;
-    const data = req.query.q ? await api(req.query.q) : {};
-
-    if (data.result && data.result.mime_type && data.result.mime_type.includes('text')) {
-        text = await axios(process.env.LBRY_STREAMING_API + data.result.sd_hash);
+    if (req.query.q) {
+        console.log(req.query.q);
+        const data = await api("get", {uri: req.query.q});
+        let text;
+        if (data && data.mime_type && data.mime_type.includes('text')) {
+            text = await axios(process.env.LBRY_STREAMING_API + data.sd_hash);
+        }
+        return res.render('index.ejs', {data, text});
     }
+    if (req.query.s) {
+        const data = await api("claim_search", {text: req.query.s, page: parseInt(req.query.p || '1')});
 
-    res.render('index', {data, text});
+        // console.log(data);
+
+        return res.render('index.ejs', {data, query: req.query.s});
+    }
+    return res.render('index.ejs');
+
+    // res.render('index', {data, text});
+})
+
+
+// Maybe for future use
+
+app.post('/api/get', async (req,res)=>{
+    console.log(req.body);
+    if (!req.body.uri) return res.json({"error": "expected uri"});
+
+    const search = await api("get", req.body);
+
+    res.json(search);
+})
+
+app.post('/api/search', async (req,res)=>{
+    if (!req.body.text) return res.json({"error": "expected text"});
+
+    const search = await api("claim_search", req.body, true);
+
+    res.json(search);
 })
 
 app.listen(PORT, (err)=>{
